8574c6216c4f586b4bafb32304c856d361c5165f
Cody <alexhill318@yahoo.com>
2021-11-04 17:42:48 -0500

    new message

---
+++ app.js
@@ -0,0 +1,95 @@
+const gameOfTheYear = [
+    {
+        game: "Lawn Mower Simulator",
+        year: 2021,
+        Developer: "Curve Digital",
+        score: 80
+    },
+
+    {
+        game: "Back 4 Blood",
+        year: 2021,
+        Developer: "Turtle Rock",
+        score: 83
+    },
+
+    {
+        game: "Marvel's Guardians of the Galaxy",
+        year: 2021,
+        publisher: "Eidos Montreal",
+        score: 85
+    },
+
+    {
+        game: "Reisdent Evil Village",
+        year: 2021,
+        Developer: "Capcom",
+        score: 93,
+        winner: "Yes"
+    }
+]
+
+const bestArtDirection = [
+    {
+        game: "Life is Strange: True Colors",
+        year: 2021,
+        Developer: "Deck Nine",
+        Director: "Zak Garriss",
+        Engine: "Unreal Engine 4"
+    },
+
+    {
+        game: "DeathLoop",
+        year: 2021,
+        Developer: "Arkane Studios",
+        Director: "Dinga Bakaba",
+        Engine: "Void Engine"
+    },
+
+    {
+        game: "Marvel's Guardians of the Galaxy",
+        year: 2021,
+        Developer: "Eidos Montreal",
+        Director: "Jean-Francois Dugas, Patrick Fortier",
+        Engine: "Dawn Engine "
+    },
+
+    {
+        game: "Kena: Bridge of Spirits",
+        year: 2021,
+        Developer: "Ember Lab",
+        Director: "Mike Grier",
+        Engine: "Unreal Engine 4"
+    },
+
+]
+
+const bestGameDirection = [
+    {
+        game: "It Take Two",
+        Year: 2021,
+        Developer: "Electronic Arts",
+        Writer: "Soni Jorgeson, Josef Fares"
+    },
+
+    {
+        game: "Metroid Dread",
+        Year: 2021,
+        Developer: "Nintendo",
+        Writer: "Fumi Hayashi"
+    },
+
+    {
+        game: "Halo Infinite",
+        Year: 2021,
+        Developer: "343 Industries",
+        Writer: "Paul Crocker"
+    },
+
+    {
+        game: "Resident Evil Village",
+        Year: 2021,
+        Developer: "Capcom",
+        Writer: "Antony Johnston"
+    }
+]
